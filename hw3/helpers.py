ac5ca8f87652668ed915a5543b88347015687d93
Rohith <rohith2017324@gmail.com>
2020-02-29 18:23:48 -0500

    implemented 1/2 hog hw3

--- hw3/helpers.py
+++ hw3/helpers.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def convolve(im_arr, f, pad=False):
+def convolve(im_arr, f, pad=True):
 	img_out = np.zeros(im_arr.shape)
 
 	if len(f.shape) != 1: f.shape = (f.shape[0], f.shape[1])
@@ -19,7 +19,7 @@ def convolve(im_arr, f, pad=False):
 	for i in range(1, im_arr.shape[0]-fltr_sz_y-1):
 		for j in range(1, im_arr.shape[1]-fltr_sz_x-1):
 			value =  np.multiply(f, im_arr[(i - 1):(i + fltr_sz_x-1), (j - 1):(j + fltr_sz_y-1)])
-			img_out[i, j] = value.sum ()
+			img_out[i, j] = value.sum()
 
 	return img_out.astype(np.float32)
 
@@ -35,5 +35,4 @@ def gradient_calc(img):
 
 	E_s = np.sqrt(np.add(J_x**2, J_y**2))
 	E_o = np.arctan2(J_y, J_x) * 180 / np.pi
-	E_o[E_o < 0] += 180
-	return E_s, E_o
+	return E_s, np.abs(E_o)
--- hw3/hog.py
+++ hw3/hog.py
@@ -4,14 +4,41 @@ from PIL import Image, ImageDraw
 import math as m
 import matplotlib.pyplot as plt
 
-import helpers
+import helpers as hp
 
-im = Image.open("./input.jpg")
-# print(im.size)
-im = im.convert('L')
+IMG_SIZE = 400
+im = Image.open("input2.jpg").resize((IMG_SIZE,IMG_SIZE)).convert('L')
 img = np.array(im).astype(np.float32)
 
-M, D = helpers.gradient_calc(img)
+M, D = hp.gradient_calc(img)
+# M, D = M.astype(np.uint16), D.astype(np.uint16)
 
-img_out = Image.fromarray(np.uint8(M)).convert('L')
-img_out.save("output.jpg")
+N_BUCKETS = 9
+CELL_SIZE = 8
+def get_cell_histogram(cell_M, cell_D):
+	buckets = np.linspace(0, 180, N_BUCKETS + 1)
+	bucket_vals = np.zeros(N_BUCKETS)
+	for i in range(0, CELL_SIZE):
+		for j in range(0, CELL_SIZE):
+			m = cell_M[i,j]
+			d = cell_D[i,j]
+			left_bin = int(d / 20.)
+			right_bin = (int(d / 20.) + 1) % N_BUCKETS
+			right_val = m * (d - left_bin * 20) / 20
+			left_val= m - right_val
+			bucket_vals[left_bin] += left_val
+			bucket_vals[right_bin] += right_val
+	return bucket_vals
+
+def make_cells(M, D):
+	n = int(IMG_SIZE / CELL_SIZE)
+	cells = np.empty((n, n), dtype=np.ndarray)
+	for i in range(0, M.shape[0], CELL_SIZE):
+		for j in range(0, M.shape[1], CELL_SIZE):
+			cell_M = M[i:i+CELL_SIZE, j:j+CELL_SIZE]
+			cell_D = D[i:i+CELL_SIZE, j:j+CELL_SIZE]
+			cells[int(i/CELL_SIZE), int(j/CELL_SIZE)] = get_cell_histogram(cell_M, cell_D)
+	return cells
+
+cells = make_cells(M,D)
+img_out = Image.fromarray(np.uint8(M)).convert('L').save('output.jpg')
